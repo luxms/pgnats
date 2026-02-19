f42f4a6bc62d7e81a8c335b3de6130bc73410142
if0ne <pavel.agafonov.al@gmail.com>
2026-02-19 17:23:14 +0300

    docs: update mdbook
    Signed-off-by: if0ne <pavel.agafonov.al@gmail.com>

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5eb97b4b.js',
+                path_to_root + 'searchindex-0ca935cf.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
